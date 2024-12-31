9b2b7c17adc73068e9ef5a056da1458968cd2035
juan <juanjaimes10010@gmail.com>
2024-12-30 21:12:39 -0600

    freeCodeCamp Project 2

--- index.js
+++ index.js
@@ -8,25 +8,51 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const { type } = require('express/lib/response');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.use('/', (req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next()
+});
+
+
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+
+app.get("/api/", function (req, res) {
+  res.json({ "unix": new Date().getTime(), "utc": new Date().toUTCString() });
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// app.get("/api/:date?", (req, res) => {
+
+//   const data = /^\d+$/.test(req.params.date) ? parseInt(req.params.date) : req.params.date ?? Date.now();
+
+//   const date = new Date(data);
+
+//   if (date == "Invalid Date") res.json({ error: "Invalid Date" });
+
+//   res.json({ unix: parseInt(date.getTime()), utc: date.toUTCString() });
+
+
+// })
+
+
+app.get("/api/whoami", (req, res) => {
+
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  })
+})
 
 
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(3330, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
